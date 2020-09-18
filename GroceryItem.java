6ba8c2f8309a98dfe51a17fb6d400a60ef998094
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2020-09-18 17:26:01 -0400

    ShoppingBag, GroceryItem need testing
    ShoppingBag, GroceryItem need testing.
    
    It seems that most if not all of the following classes are done. They need testing now.

--- GroceryItem.java
+++ GroceryItem.java
@@ -1,53 +1,79 @@
 public class GroceryItem {
-	private String name; 
-	private double price; 
+	private String name;
+	private double price;
 	private boolean taxable;
 	
-	
-	public GroceryItem (String name, double price, boolean taxable) {
+	/**
+	 * Initializes values of a new GroceryItem.
+	 * @param name Name of the GroceryItem.
+	 * @param price Price of the GroceryItem.
+	 * @param taxable True if GroceryItem is taxable, false otherwise.
+	 */
+	public GroceryItem(String name, double price, boolean taxable) {
 		this.name = name;
 		this.price = price;
 		this.taxable = taxable;
 	}
 	
+	/**
+	 * Gets the name of a GroceryItem.
+	 * @return name of GroceryItem.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Gets the price of a GroceryItem.
+	 * @return price of the GroceryItem.
+	 */
 	public double getPrice() {
 		return price;
 	}
-	
+
+	/**
+	 * Gets whether a GroceryItem is taxable or not. 
+	 * @return true if taxable, false non-taxable.
+	 */
 	public boolean getTaxable() {
 		return taxable;
 	}
 	
-	public boolean equals(Object obj){
+	/**
+	 * Method returns true only if all the data field values of the two objects are the same.
+	 * @param obj potential GroceryItem to compare. 
+	 * @return true if GroceryItems are equal, false otherwise.
+	 */
+	public boolean equals(Object obj) {
 		if (obj instanceof GroceryItem) {
-			GroceryItem item = (GroceryItem) obj;
-			System.out.println(item.toString());
+			GroceryItem item = (GroceryItem) obj;//cast to grocery item WHY?
+
+			if((this.name == item.name) && (this.price == item.price) && (this.taxable == item.taxable)) {
+				return true;
+			}
 		}
-		else {
-			return false;
-		}
-		/**
-		 * returns true only if all the data field values of the two objects are the same
-		 */
-		return true;
-	} 
-	public String toString() { 
+		return false;
+	}
+
+	/**
+	 * Gets a formatted string about the GroceryItem.
+	 * @return itemName: $xx.xx : tax status.
+	 */
+	public String toString() {
 		return getName() + ": $" + String.format("%.2f", getPrice()) + " : " + getTaxable();
 	}
 
 	public static void main(String[] args) {
 		GroceryItem item1 = new GroceryItem("milk", 2.00, false);
-		System.out.println(item1);
-		
-		GroceryItem item2 = new GroceryItem("milk", 2.00, false);
+		GroceryItem item2 = new GroceryItem("milk", 2.01, false);
+
+		//test equality method
 		System.out.println(item2.equals(item1));
-		
+		System.out.println(item1.equals(item2));
+		System.out.println(item2.equals(item2));
+
+
 
 	}
 
 }
-
--- ShoppingBag.java
+++ ShoppingBag.java
@@ -1,38 +1,143 @@
-
 public class ShoppingBag {
-	public ShoppingBag() { }
-	private int find(GroceryItem item) {
-		return 0; 
-		
-	} // helper method to find an item 
-	private void grow() { //grow size by 5 when bag is full
-		
-	} // helper method to grow the capacity
+	private GroceryItem[] bag; // array-based implementation of the bag
+	private int size; // number of items currently in the bag
 	
-	public void add(GroceryItem item) { 
-		
-	}
-	
-	public boolean remove(GroceryItem item) {
-		return false; 
+	/**
+	 * Constructor for ShoppingBag, initializes to default sizes.
+	 */
+	public ShoppingBag() {
+		this.bag = new GroceryItem [5];
+		this.size = 0;
 		
 	}
-	
-	public double salesPrice() {
-		return 0; 
-		
-	}
-	
-	public double salesTax() {
-		return 0; 
-		
-	}
-	
-	public void print() { }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	/**
+	 * Search the ShoppingBag for a specific GroceryItem.
+	 * @param item GroceryItem that we are searching for in the ShoppingBag.
+	 * @return index of the GroceryItem we were searching for.
+	 */
+	private int find(GroceryItem item) {
+		for(int i = 0; i < size; i++) {
+			if(bag[i].equals(item)) {
+				return i;
+			}
+		}
+		return -1;
+
+	} // helper method to find an item
+
+	/**
+	 * Helper method to make ShoppingBag larger by 5.
+	 */
+	private void grow() { // grow size by 5 when bag is full
+		GroceryItem[] tempBag = new GroceryItem[size + 5];
+		for (int i = 0; i < size; i++) {
+			GroceryItem t = bag[i];
+			tempBag[i] = t;
+		}
+		bag = tempBag;
+	} // helper method to grow the capacity
+
+	/**
+	 * Adds a GroceryItem to the shopping bag.
+	 * @param item GroceryItem to be added to the bag.
+	 */
+	public void add(GroceryItem item) {
+		if(size < bag.length) {
+			bag[size] = item;
+			size++;
+			return;
+		}
+		grow();
+		add(item);
+		return;
+		
+	}
+
+	/**
+	 * Removes the first instance of item from the ShoppingBag.
+	 * If there are more than one matching item, 
+	 * remove the first item found in the bag.
+	 * @param item Item to be removed from the ShoppingBag.
+	 * @return True if the remove was successful, false otherwise.
+	 */
+	public boolean remove(GroceryItem item) {
+		int itemIndex = find(item);
+		
+		if(itemIndex == -1) {
+			return false;
+		}
+		
+		bag[itemIndex] = bag[size - 1];
+		bag[size - 1] = null;
+		size --;
+		return true;
+	}
+
+	/**
+	 * Price of all items in the ShoppingBag before tax is applied.
+	 * @return Total cost of items before tax.
+	 */
+	public double salesPrice() {
+		double salesPrice = 0;
+		
+		for(int i = 0; i < size; i++) {
+			salesPrice += bag[i].getPrice();
+		}
+		
+		return salesPrice;
+
+	}
+
+	/**
+	 * Total sales tax of all items in the ShoppingBag.
+	 * @return Sales tax in the form of a double.
+	 */
+	public double salesTax() {
+		double salesTax = 0;
+		
+		for(int i = 0; i < size; i++) {
+			if(bag[i].getTaxable()) {
+				salesTax += (bag[i].getPrice() * 0.06625) ;
+			}
+		}
+		
+		return salesTax;
+	}
+
+	/**
+	 * Prints number of items and contents of shoppingBag.
+	 */
+	public void print() {
+		System.out.println("**You have " + size + " items(s) in the bag:");
+		
+		for(int i = 0; i < size; i++) {
+			System.out.println(bag[i].toString());
+		}
+		
+		System.out.println("**End of list");
+	}
+
+	public static void main(String[] args) {
+		GroceryItem item1 = new GroceryItem("milk", 2.00, true);
+		GroceryItem item2 = new GroceryItem("not milk", 2.00, false);
+
+		ShoppingBag arr = new ShoppingBag();
+		arr.add(item1);
+		arr.add(item1);
+		arr.add(item2);
+		arr.add(item1);
+		arr.add(item1);
+		arr.add(item1);
+
+		arr.remove(item2);
+
+		arr.print();
+		
+		System.out.println("Sales Price: " + arr.salesPrice());
+		System.out.println("Sales Tax: " + arr.salesTax());
+
+		
 	}
 
 }
