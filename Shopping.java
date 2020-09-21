e1dad09408f40661b2d5b45555c2d5fc7eea320e
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2020-09-21 15:12:35 -0400

    Created Shopping class
    skeleton of shopping class

---
+++ Shopping.java
@@ -0,0 +1,14 @@
+/**
+ * This class is the user interface class that handles the input commands, 
+ * output data and messages.
+ * @author Joseph Hawkins, Gustavo Garcia
+ *
+ */
+public class Shopping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ShoppingBag.java
+++ ShoppingBag.java
@@ -50,8 +50,7 @@ public class ShoppingBag {
 		}
 		grow();
 		add(item);
-		return;
-		
+		return;	
 	}
 
 	/**
