76a0ff339abad4aa26359a080a1be51eafe07a39
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2020-09-21 15:53:12 -0400

    Update Shopping.java
    started

--- Shopping.java
+++ Shopping.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This class is the user interface class that handles the input commands, 
  * output data and messages.
@@ -6,12 +8,39 @@
  */
 public class Shopping {
 
-	public void run() {
-		
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public void run() {
 
-	}
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Let's start shopping!");
+
+        String input = sc.nextLine();
+
+        while ( !"Q".equals(input)) {
+
+            String[] splitted = input.split("\\s+");
+
+            char command = splitted[0].charAt(0);
+            
+            switch(command) {
+            case 'A':
+            	System.out.println("A**");
+            }
+            //if(splitted[0].equals())
+            System.out.println("I WORK");
+
+            input = sc.nextLine();
+        }
+
+        sc.close();
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Shopping item = new Shopping();
+
+        item.run();
+
+    }
 
 }
