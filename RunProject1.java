0ebd0be2cd3d834a2e4af03c587ca21b34bcbe0f
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2020-09-24 12:16:35 -0400

    Created Project 1 driver
    Creates a Shopping object and calls the run method.

---
+++ RunProject1.java
@@ -0,0 +1,12 @@
+/**
+ * Driver class for Project 1. 
+ * @author Joseph Hawkins, Gustavo Garcia
+ *
+ */
+public class RunProject1 {
+
+	public static void main(String[] args) {
+		new Shopping().run();
+	}
+
+}
